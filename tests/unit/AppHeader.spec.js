f7da89ace5b5833b7053fa848752874cda5e60bb
Mikulew <mikulew@gmail.com>
2021-04-25 17:47:30 +0200

    Add AppHeader test

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,11 +1,19 @@
 import AppHeader from '@/components/AppHeader'
+import { FLIPPED_ALIAS_KEYS } from '@babel/types'
+import { mount } from '@vue/test-utils'
 
 describe('AppHeader', () => {
   test('If user is not logged in, do not show logout button', () => {
-    expect(true).toBe(true)
+    const wrapper = mount(AppHeader)
+
+    expect(wrapper.find('button').isVisible()).toBe(false)
   })
 
-  test('If user is logged in, show logout button', () => {
-    expect(true).toBe(true)
+  test('If user is logged in, show logout button', async () => {
+    const wrapper = mount(AppHeader)
+    wrapper.setData({ loggedIn: true })
+
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('button').isVisible()).toBe(true)
   })
 })
